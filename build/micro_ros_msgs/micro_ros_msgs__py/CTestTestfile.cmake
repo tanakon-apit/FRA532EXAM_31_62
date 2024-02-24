dc51c29a1bf4ded04645be62bd5ddc7e098c43aa
tanakon-apit <08809kpn@gmail.com>
2024-02-24 19:52:36 +0700

    commit

---
+++ build/micro_ros_msgs/micro_ros_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tanakon/FRA532EXAM_31_62_WS/build/micro_ros_msgs/micro_ros_msgs__py
+# Build directory: /home/tanakon/FRA532EXAM_31_62_WS/build/micro_ros_msgs/micro_ros_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
