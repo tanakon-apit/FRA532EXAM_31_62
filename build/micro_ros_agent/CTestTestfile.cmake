d651afb6deb43a6cb1af644c4a282e3af7c18b47
tanakon-apit <08809kpn@gmail.com>
2024-02-24 19:55:03 +0700

    commit micro_ros firmware

--- build/micro_ros_agent/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tanakon/FRA532EXAM_31_62_WS/src/uros/micro-ROS-Agent/micro_ros_agent
-# Build directory: /home/tanakon/FRA532EXAM_31_62_WS/build/micro_ros_agent
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
