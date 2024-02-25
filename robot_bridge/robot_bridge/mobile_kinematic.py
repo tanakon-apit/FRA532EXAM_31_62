f1c7864a6896419b808bf32ea4872153d86ca9d0
tanakon-apit <08809kpn@gmail.com>
2024-02-26 01:25:58 +0700

    odom covariance estimator

--- robot_bridge/robot_bridge/mobile_kinematic.py
+++ robot_bridge/robot_bridge/mobile_kinematic.py
@@ -10,19 +10,16 @@ class Diff_Drive_Kinematic():
                               0.0, 0.0, 
                               -r / b, r / b]).reshape(3, 2)
         self.pos_g = np.zeros(3)
-        self.qd = np.zeros(2)
 
     def get_wheelspeed(self, vel_r : list):
-        self.qd = self.Tt2w @ np.array(vel_r)
-        return self.qd
-
-    def get_pose(self, dt : float):
+        return self.Tt2w @ np.array(vel_r)
+    
+    def get_pose(self, qd : np, dt : float):
         Rr2g = np.array([np.cos(self.pos_g[2]), -np.sin(self.pos_g[2]), 0.0,
                          np.sin(self.pos_g[2]), np.cos(self.pos_g[2]), 0.0,
                          0.0, 0.0, 1.0]).reshape(3, 3)
-        vel_g = Rr2g @ self.Tw2t @ self.qd
-        self.pos_g += vel_g * dt
+        self.pos_g += (Rr2g @ self.Tw2t @ qd) * dt
         return self.pos_g
 
-    def get_twist(self):
-        return np.linalg.inv(self.Tt2w) @ self.qd
+    def get_twist(self, qd : np):
+        return np.linalg.inv(self.Tt2w) @ qd
