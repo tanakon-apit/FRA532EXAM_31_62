fix covariance estimator

robot_bridge/robot_bridge/mobile_cov_estimator.py
@@ -12,8 +12,8 @@ class Diff_Drive_Cov_Estimator():
         ds = (dsr + dsl) / 2.0
         dtheta = (dsr - dsl) / 2.0
         s_cov = self.k
-        s_cov[0][0] *= dsr
-        s_cov[1][1] *= dsl
+        s_cov[0][0] *= np.abs(dsr)
+        s_cov[1][1] *= np.abs(dsl)
 
         a0 = np.cos(theta + (dtheta / 2.0))
         a1 = np.sin(theta + (dtheta / 2.0))

robot_bridge/scripts/diff_drive_robot.py
@@ -21,7 +21,7 @@ class DiffDriveRobot(Node):
         self.create_timer(0.033, self.timer_callback)
 
         self.Kine = Diff_Drive_Kinematic(r=0.03375, b=0.1625)
-        self.Cov = Diff_Drive_Cov_Estimator(kr=1.0, kl=1.0)
+        self.Cov = Diff_Drive_Cov_Estimator(kr=1.0e-3, kl=1.0e-3)
         self.qd = np.zeros(2)
         self.pose = np.zeros(3)
         self.twist = np.zeros(2)