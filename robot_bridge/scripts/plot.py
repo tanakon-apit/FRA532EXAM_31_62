8e7a71dcd1cd9835d303e946f9ded966defb3097
tuchapong1234 <113016544+tuchapong1234@users.noreply.github.com>
2024-02-29 23:43:13 +0700

    update readme

--- robot_bridge/scripts/plot.py
+++ robot_bridge/scripts/plot.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 # data = pd.read_csv('/home/tanakon/FRA532EXAM_31_62_WS/university_records.csv')
-data = pd.read_csv('/home/tanakon/FRA532EXAM_31_62_WS/half_circle.csv')
+data = pd.read_csv('/home/tuchapong1234/FRA532EXAM_WS/src/circle.csv')
 
 
 fig, ax = plt.subplots()
@@ -15,9 +15,9 @@ x2 = np.array(data['odom_filter_x'])
 y2 = np.array(data['odom_filter_y'])
 x3 = np.array(data['cmd_x'])
 y3 = np.array(data['cmd_y'])
-x4 = (np.array(data['ground_x']) - 6.068770e-2) * 0.929008008169053
+x4 = (np.array(data['ground_x']) - 0.0553) * 0.929008008169053
 
-y4 = (np.array(data['ground_y']) - 6.588850e-1) * 0.91784356538233
+y4 = (np.array(data['ground_y']) - 0.657) * 0.91784356538233
 
 
 # plt.plot(x1, y1, marker='o', linestyle='', label='Dataset 1')
@@ -30,7 +30,7 @@ plt.plot(x4, y4, label='ground')
 
 plt.xlabel('X Axis Label')
 plt.ylabel('Y Axis Label')
-plt.title('Multiple Datasets Plot')
+plt.title('Circle Test Plot')
 plt.legend()  # Show legend
 
 ax.set_aspect(1)
